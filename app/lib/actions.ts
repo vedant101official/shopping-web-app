8eab18a7a99f9186975504c40155fbce34bd6472
vedant101official <vedant101official@gmail.com>
2026-01-15 15:19:26 +0530

    auth and meta data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,8 +3,10 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export type State = {
     errors?: {
@@ -119,4 +121,23 @@ export async function deleteInvoice(id: string) {
     }
     revalidatePath('/dashboard/invoices');
 
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
